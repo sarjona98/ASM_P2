06e1b01087ab3557c50bec99ca77a2175ede5ccc
sarjona98 <sarjona98@gmail.com>
2020-04-04 20:56:24 +0200

    Initial commit

---
+++ app/src/test/java/cat/urv/deim/asm/p2/common/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cat.urv.deim.asm.p2.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
