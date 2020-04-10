916fb9a972b9c9d529ca0a67c3a3bf1fd3911e41
sarjona98 <sarjona98@gmail.com>
2020-04-10 17:02:34 +0200

    Versión inicial de SplashActivity

---
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -0,0 +1,27 @@
+package cat.urv.deim.asm.p2.common;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
