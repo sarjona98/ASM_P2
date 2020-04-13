c19ed21ab63305583e119ed1e05c3c25ff621868
Robert Beltri Pinel <robert.beltri@estudiants.urv.cat>
2020-04-13 14:08:46 +0200

    versió inicial LoginActivity

--- app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
+import cat.urv.deim.asm.p2.common.ui.login.LoginActivity;
+
 public class SplashActivity extends Activity {
 
     @Override
@@ -18,7 +20,7 @@ public class SplashActivity extends Activity {
 
         new Handler().postDelayed(new Runnable(){
             public void run(){
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
