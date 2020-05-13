7b8d95d15d09f794799f5c086f3d1990cbc20673
Robert Beltri Pinel <robert.beltri@estudiants.urv.cat>
2020-05-13 10:59:34 +0200

    afegir ActionBar amb Back al ErrorLoginActivity

--- app/src/main/java/cat/urv/deim/asm/p2/common/ErrorLoginActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/ErrorLoginActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Objects;
 
 import cat.urv.deim.asm.p2.common.ui.login.LoginActivity;
 
@@ -16,6 +20,9 @@ public class ErrorLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error_login);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
         final Button try_againButton = findViewById(R.id.try_again);
 
@@ -28,6 +35,25 @@ public class ErrorLoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        search.setVisible(false);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
